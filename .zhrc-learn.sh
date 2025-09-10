5fd4661d04f0d6be828c9ff651c55a1351cce441
chris <caaker.0@gmail.com>
2025-09-10 12:04:29 -0500

    auto-commit

--- .zhrc-learn.sh
+++ .zhrc-learn.sh
@@ -1,29 +1,32 @@
 #
 #
+#
+#
+#
+
 # C
-#
-#
-
-
 # gcc compile and run a single file, $1 holds argument #1
 gcr() {
   gcc $1
   ./a.out
 }
 
+# C++
+# g++ compile and run a single file, $1 holds argument #1
+gpcr() {
+  g++ $1
+  ./a.out
+}
 
-#
-#
 # Java
-#
-#
-
-
 # java compile and run a single class
+# javac compiles to bytecode and then java interprets the bytecode
+# if needed install the java sdk - sudo apt install default-jdk
 jcr() {
   javac "$1" -d classes
-  # java -cp classes/
+  java -cp classes "${1%.java}"
 }
+
 
 
 # java compile and run a multiple classes
